152df32d53229581bd01d71dd6e4b28e1f0e909c
Kevin Tran <kinzerfest@gmail.com>
2019-08-14 01:00:49 +0000

    rookie cookie

--- src/main/java/org/minutebots/frc2019/Robot.java
+++ src/main/java/org/minutebots/frc2019/Robot.java
@@ -1,34 +1,34 @@
 package org.minutebots.frc2019;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
 
 public class Robot extends TimedRobot {
   /**
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
    */
-  @Override
-  public void robotInit() {
-  }
 
-  @Override
-  public void autonomousInit() {
-  }
+Spark leftmotor = new Spark(0);
+Spark rightmotor = new Spark (1);
+Joystick joystick = new Joystick (0);
+DifferentialDrive drivetrain = new DifferentialDrive(leftmotor, rightmotor);
 
-  @Override
-  public void autonomousPeriodic() {
-  }
-
-  @Override
-  public void teleopInit() {
-  }
 
   @Override
   public void teleopPeriodic() {
+    drivetrain.curvatureDrive(-joystick.getY(), joystick.getX(),joystick.getRawButton(1));
   }
 
   @Override
   public void testInit() {
+    LiveWindow.add(leftmotor);
+    LiveWindow.add(rightmotor);
+    LiveWindow.add(drivetrain);
   }
 
   @Override
