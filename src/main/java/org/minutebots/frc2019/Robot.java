9c9a6220ecc56b688ec1bd7cf29f8a096582cc40
a2332 <45018361+a2332@users.noreply.github.com>
2019-09-12 16:45:25 -0500

    pid turn

--- src/main/java/org/minutebots/frc2019/Robot.java
+++ src/main/java/org/minutebots/frc2019/Robot.java
@@ -1,11 +1,13 @@
 package org.minutebots.frc2019;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 
 public class Robot extends TimedRobot {
 
@@ -14,17 +16,54 @@ Spark rightmotor = new Spark (1); //Declaring second motor with a different port
 Joystick joystick = new Joystick (0); //Declaring our joystick so we can drive our robot. Port is an integer that is required by a constructor.
 DifferentialDrive drivetrain = new DifferentialDrive(leftmotor, rightmotor); //DifferentialDrive is a controller that translates joystick input into robot movement.
 //The constructor requires 2 SpeedControllers, and Spark is a type of SpeedController so it is able to be passed in.
-
+private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+private boolean buttonIsPressed = false;
   @Override
   public void teleopPeriodic() { //Code in the brackets gets executed every 20ms.
-    drivetrain.curvatureDrive(-joystick.getY(), joystick.getX(),joystick.getRawButton(1)); 
+    
+    if(joystick.getRawButtonPressed(2) && !buttonIsPressed){
+      saneDrive.setSetpoint(gyro.getAngle());
+      buttonIsPressed = true;
+    }
+    if(joystick.getRawButtonReleased(2))
+      buttonIsPressed = false;
+
+    if(joystick.getRawButton(2)){
+      drivetrain.arcadeDrive(joystick.getY(), getOutput());
+    }
+    else
+      drivetrain.curvatureDrive(joystick.getY(), joystick.getX(), joystick.getRawButton(1));
     //We're passing in input into the this method, it needs it to be able to execute and move the motors.
   }
 
   @Override
-  public void testInit() { //This adds all the motors to a user interface called Smart Dashboard (we use Shuffleboard, which is compatible), so we can control each motor manually.
+  public void testPeriodic() {
+    SmartDashboard.putNumber("Gyro Angle", gyro.getAngle());  
+  }
+
+  @Override
+  public void robotInit() {
+    saneDrive.enable();
+    gyro.calibrate();
     SmartDashboard.putData(leftmotor);
     SmartDashboard.putData(rightmotor);
     SmartDashboard.putData(drivetrain);
+    SmartDashboard.putData(saneDrive);
   }
+
+  private double Kp = .05;
+  private double Ki = 0;
+  private double Kd = 0;
+  double output = 0;
+  PIDOutput putout = new PIDOutput(){
+    @Override
+    public void pidWrite(double input) {
+      output = input;
+    }
+  };  
+  public double getOutput(){
+    return output;
+  }
+  PIDController saneDrive = new PIDController(Kp, Ki, Kd, gyro, putout);
+
 }
